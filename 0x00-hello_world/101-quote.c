b984e3381ef53148a059c788a5ced56880508378
Bri-an0 <brianlotsu@gmail.com>
2023-02-16 16:06:11 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  UNIX TASK
+ *
+ * Return: value 1
+ */
+
+int main(void)
+{
+	echo "\"and that piece of art is useful" - Dora Korpar, \"2015-10-19\"\n
+		return = (1)
+}
